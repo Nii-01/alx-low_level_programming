4ec551419d6e20141a0034635065f4d1a330d4ed
Nii-01 <jeffreytagoe24@gmail.com>
2023-05-02 13:31:23 -0700

    C - More singly linked lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,11 +3,7 @@
 /**
  * free_listint2 - frees a linked list of type listint_t and sets the head pointer to NULL
  * @head: pointer to the first node in the list to be freed
-This function frees all the memory allocated to the linked list pointed to by @head.
-It starts by traversing the linked list starting from the first node pointed to by @head,
-and frees the memory for each node one by one until it reaches the end of the list. After
-all the memory has been freed, it sets the @head pointer to NULL to indicate that the list
-is empty
+ * This function frees all the memory allocated to the linked list pointed to by @head. It starts by traversing the linked list starting from the first node pointed to by @head, and frees the memory for each node one by one until it reaches the end of the list. After all the memory has been freed, it sets the @head pointer to NULL to indicate that the list is empty
  *If @head is NULL, this function does nothing
  */
 void free_listint2(listint_t **head)
